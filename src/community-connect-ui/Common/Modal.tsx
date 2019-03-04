0696b94320e98eedd9f1e486746b34c61face948
Alex Hughes <alxhghs@gmail.com>
2019-03-04 14:45:03 -0500

    emotion-and-css-grid: add react-onclickoutside

--- src/community-connect-ui/Common/Modal.tsx
+++ src/community-connect-ui/Common/Modal.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styled from "@emotion/styled";
+import onClickOutside from "react-onclickoutside";
 
 type StyledModalProps = {
     isOpen: boolean;
 };
 
 const StyledModal = styled("div")<StyledModalProps>`
-    display: grid;
     display: ${props => props.isOpen === true ? "grid" : "none"};
+    position: absolute;
     padding: 10px;
 `;
 
@@ -21,7 +22,15 @@ type State = {
     isOpen: boolean;
 }
 
-export class Modal extends React.Component<Props, State> {
+class ModalComponent extends React.Component<Props, State> {
+    public state = {
+        isOpen: true
+    }
+    public handleClickOutisde = (event: any) => {
+        this.setState({
+            isOpen: false,
+        })
+    }
     public render() {
         return (
             <StyledModal isOpen={this.state.isOpen}>
@@ -36,3 +45,5 @@ export class Modal extends React.Component<Props, State> {
         )
     }
 }
+
+export const Modal = onClickOutside(ModalComponent);
